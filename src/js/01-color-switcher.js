8a9007cf7fcf11335541248101e723c5d25cf0cb
Serhii Rybak <rybaksw@gmail.com>
2022-05-11 18:58:20 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+let idInterval = null;
+
+btnStart.addEventListener('click', onBtnStart);
+btnStop.addEventListener('click', onBtnStop);
+
+btnStop.setAttribute('disabled', true);
+
+function onBtnStart() {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+
+  idInterval = setInterval(() => {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+}
+
+function onBtnStop() {
+  clearInterval(idInterval);
+
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
