333a3dad1a1e1cc6cd7f13bb11074b48f2e709f9
Serhii Rybak <rybaksw@gmail.com>
2022-05-12 17:19:31 +0300

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onBtnCreateClick);
+
+function onBtnCreateClick(e) {
+  e.preventDefault();
+
+  const data = {
+    delay: Number(form.delay.value),
+    step: Number(form.step.value),
+    amount: Number(form.amount.value),
+  };
+
+  let nextDelay = data.delay;
+
+  for (let i = 1; i <= data.amount; i++) {
+    createPromise(i, nextDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+          timeout: 2000,
+        });
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+          timeout: 2000,
+        });
+      });
+
+    nextDelay += data.step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
