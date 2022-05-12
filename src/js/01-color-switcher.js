a3aaf587720a300777bef68d919a0a720b95f0d4
Serhii Rybak <rybaksw@gmail.com>
2022-05-12 18:16:45 +0300

    edit name

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -25,7 +25,7 @@ function onBtnStop() {
   btnStart.removeAttribute('disabled');
   btnStop.setAttribute('disabled', true);
 }
-//
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
